3c6b3839c18c4321f6215aee9e5c813d752c1ee5
Squeakers99 <64871329+Squeakers99@users.noreply.github.com>
2023-04-19 11:53:18 -0400

    Scenes 4/5/6/7
    Scenes 4, 5, 6, and 7 created
    
    Changed the functions to void functions

--- MidtermProject.java
+++ MidtermProject.java
@@ -16,25 +16,95 @@ public class MidtermProject{
 		int intMouseY;
 		int intMouseButtonClicked;
 		
+		//Defines the variables for random number inputs
+		double dblRandomNumber1;
+		double dblRandomNumber2;
+		double dblAnswer;
+		double dblAnswerInput;
+		
+		//Initializes the variables for random number inputs
+		dblRandomNumber1 = 0.0;
+		dblRandomNumber2 = 0.0;
+		dblAnswer = 0.0;
+		dblAnswerInput = 0.0;
+		
 		//Initializes all the mouse variables
 		intMouseX = 0;
 		intMouseY = 0;
 		intMouseButtonClicked = 0;
 		
-		//Runs scene 1 to get the users choice and then clears the console
-		strChoice = scene1(con);
+		//Draws Scene 1
+		scene1(con);
+		
+		//Repeats until they provide yes or no
+		while(!strChoice.equalsIgnoreCase("yes") && !strChoice.equalsIgnoreCase("no")){
+			con.print("Do you want to join robotics?\n> ");
+			strChoice = con.readLine();
+			if(!strChoice.equalsIgnoreCase("yes") && !strChoice.equalsIgnoreCase("no")){
+				con.println("Your choice isn't valid. Please provide a valid choice.");
+			}
+		}
+		
+		//Clears the console for the next scene
 		con.clear();
 		
 		//If the player chooses no, this code runs
 		if(strChoice.equalsIgnoreCase("no")){
+			//Draws Scene 2
 			scene2(con);
 		}
 		
 		//Runs choice menu with the options for scene 3
 		strChoice = scene3(con,intMouseX,intMouseY,intMouseButtonClicked);
 		
-		//Testing if the user can select a choice
-		con.println(strChoice);
+		//Clears the console for the next scene
+		con.clear();
+		
+		//If the player chooses programming, this code runs
+		if(strChoice.equals("Programming")){
+			//Randomizes the 2 numbers
+			dblRandomNumber1 = (Math.round(Math.random()*100))/10.0;
+			dblRandomNumber2 = Math.round(Math.random()*100);
+		
+			//Gets the answer
+			dblAnswer = (Math.round((dblRandomNumber1*dblRandomNumber2)*10.0))/10.0;
+			
+			//Draws out the scene
+			scene4(con,dblRandomNumber1,dblRandomNumber2);
+			
+			//Gets the players answer
+			dblAnswerInput = con.readDouble();
+			
+			//Clears the console for the next scene
+			con.clear();
+			
+			//If the player answers wrong, this code runs
+			if(dblAnswer != dblAnswerInput){
+				//Re-randomizes the 2 numbers
+				dblRandomNumber1 = (Math.round(Math.random()*100))/10.0;
+				dblRandomNumber2 = Math.round(Math.random()*100);
+			
+				//Gets the answer
+				dblAnswer = (Math.round((dblRandomNumber1*dblRandomNumber2)*10.0))/10.0;
+				
+				//Draws out the scene
+				scene6(con,dblRandomNumber1,dblRandomNumber2);
+				
+				//Gets the players answer
+				dblAnswerInput = con.readDouble();
+				
+				//Clears the console for the next scene
+				con.clear();
+			
+				//If the player answers wrong again, this code runs
+				if(dblAnswer != dblAnswerInput){
+					dissapointedFabroa(con);
+				}
+			}
+			
+			//If the answer is right, this code runs
+			impressedFabroa(con);
+		}
 	}
 	
 	//Choice menu method that returns the choice that has been clicked (used for scenes 3, 8, 18, 19, and 20)
@@ -200,44 +270,62 @@ public class MidtermProject{
 		}
 	}
 	
-	//Method for Scene 1 (Do you want to join robotics)
-	public static String scene1(Console con){
-		//Defines and loads in the image for scene 1
-		BufferedImage sceneImage;
-		sceneImage = con.loadImage("Scene 1.png");
+	//Method to draw impressed fabroa (Used for scenes 5, 17)
+	public static void impressedFabroa(Console con){
+		//Defines and loads the image for this scene
+		BufferedImage imgImpressedFabroa;
+		imgImpressedFabroa = con.loadImage("Impressed Fabroa.png");
 		
-		//Defines and initializes the string variable to be used for the users choice
-		String strChoice;
-		strChoice = "";
+		//Draws the scene image
+		con.drawImage(imgImpressedFabroa,0,200);
+		
+		//Prints a description of the scene
+		con.println("Mr. Fabroa is impressed with your skills");
+		
+		//Pauses the console for 5 seconds
+		con.sleep(5000);
+	}
+	
+	//Method to draw dissapointed fabroa (Used for scenes 7,10,15,21,25)
+	public static void dissapointedFabroa(Console con){
+		BufferedImage imgDissapointedFabroa;
+		imgDissapointedFabroa = con.loadImage("Disappointed Fabroa.png");
+		
+		//Draws a dissapointed Fabroa face
+		con.drawImage(imgDissapointedFabroa,0,200);
+		
+		//Prints a message saying you can do better
+		con.println("Mr. Fabroa is dissapointed in you. You can do better!");
+		
+		//Delays for 5 seconds
+		con.sleep(5000);
+		
+		//Closes the console
+		con.closeConsole();
+	}
+	
+	//Method for Scene 1 (Do you want to join robotics)
+	public static void scene1(Console con){
+		//Defines and loads in the image for scene 1
+		BufferedImage imgScene1;
+		imgScene1 = con.loadImage("Scene 1.png");
 		
 		//Draws the rectangle for the input
 		con.setDrawColor(Color.black);
 		con.drawRect(0,0,1280,200);
 		
 		//Draws the picture for the scene
-		con.drawImage(sceneImage,0,200);
-		
-		//Repeats until they provide yes or no
-		while(!strChoice.equalsIgnoreCase("yes") && !strChoice.equalsIgnoreCase("no")){
-			con.print("Do you want to join robotics?: ");
-			strChoice = con.readLine();
-			if(!strChoice.equalsIgnoreCase("yes") && !strChoice.equalsIgnoreCase("no")){
-				con.println("Your choice isn't valid. Please provide a valid choice.");
-			}
-		}
-		
-		//Returns their choice
-		return strChoice;
+		con.drawImage(imgScene1,0,200);
 	}
 	
 	//Method for Scene 2 (Hope to see you next year)
 	public static void scene2(Console con){
 		//Defines and loads in the image for scene 2
-		BufferedImage sceneImage;
-		sceneImage = con.loadImage("Scene 2.png");
+		BufferedImage imgScene2;
+		imgScene2 = con.loadImage("Scene 2.png");
 		
 		//Draws the image and prints the statement
-		con.drawImage(sceneImage,0,200);
+		con.drawImage(imgScene2,0,200);
 		con.println("We hope to see you next year!");
 		
 		con.sleep(5000);
@@ -259,5 +347,49 @@ public class MidtermProject{
 		
 		//Returns the players selected subcommittee
 		return strChoice;
+	}
+	
+	//Method for Scene 4 (Answer the equation)
+	public static void scene4(Console con,double dblRandomNumber1,double dblRandomNumber2){
+		//Defines and loads the image for this scene
+		BufferedImage imgScene4;
+		imgScene4 = con.loadImage("Scene 4.png");
+		
+		//Defines and sets the font for the equation
+		Font fntEquationFont;
+		fntEquationFont = con.loadFont("Equation Font.ttf",100);
+		
+		//Draws the scene image
+		con.drawImage(imgScene4,0,200);
+		con.setDrawColor(Color.black);
+		con.setDrawFont(fntEquationFont);
+		con.drawString(dblRandomNumber1+" * "+dblRandomNumber2+" = ?",500,400);
+		
+		//Print statements to give the user instructions
+		con.println("To be a programmer, you have to know how to do math");
+		con.println("What is the answer to this equation? (round to the nearest tenth)");
+		con.print("> ");
+	}
+	
+	//Method for Scene 6 (Last Chance)
+	public static void scene6(Console con,double dblRandomNumber1,double dblRandomNumber2){
+		//Defines and loads the image for this scene
+		BufferedImage imgScene6;
+		imgScene6 = con.loadImage("Scene 6.png");
+		
+		//Defines and sets the font for the equation
+		Font fntEquationFont;
+		fntEquationFont = con.loadFont("Equation Font.ttf",100);
+		
+		//Draws the scene image
+		con.drawImage(imgScene6,0,200);
+		con.setDrawColor(Color.black);
+		con.setDrawFont(fntEquationFont);
+		con.drawString(dblRandomNumber1+" * "+dblRandomNumber2+" = ?",500,400);
+		
+		//Print statements to give the user instructions
+		con.println("Wrong! But, you have one more chance");
+		con.println("What is the answer to this equation? (round to the nearest tenth)");
+		con.print("> ");
 	}
 }
