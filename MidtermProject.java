78be0aa1660ef9a160ab10dc12abf7edeeedd30b
Squeakers99 <64871329+Squeakers99@users.noreply.github.com>
2023-04-19 23:44:24 -0400

    Scenes 8/9/10/11/12
    Completion of the programming branch of the storyline

--- MidtermProject.java
+++ MidtermProject.java
@@ -1,3 +1,9 @@
+/*
+Soheil Rajabali
+The Robotics Adventure
+V4.0
+*/
+
 import arc.*;
 import java.awt.*;
 import java.awt.image.*;
@@ -22,6 +28,12 @@ public class MidtermProject{
 		double dblAnswer;
 		double dblAnswerInput;
 		
+		//Defines string variable for the robot minigame
+		String strResult;
+		
+		//Defines variable for the binary question
+		int intAnswer;
+		
 		//Initializes the variables for random number inputs
 		dblRandomNumber1 = 0.0;
 		dblRandomNumber2 = 0.0;
@@ -38,7 +50,8 @@ public class MidtermProject{
 		
 		//Repeats until they provide yes or no
 		while(!strChoice.equalsIgnoreCase("yes") && !strChoice.equalsIgnoreCase("no")){
-			con.print("Do you want to join robotics?\n> ");
+			con.println("Do you want to join robotics (Yes or No)?");
+			con.print("> ");
 			strChoice = con.readLine();
 			if(!strChoice.equalsIgnoreCase("yes") && !strChoice.equalsIgnoreCase("no")){
 				con.println("Your choice isn't valid. Please provide a valid choice.");
@@ -104,12 +117,50 @@ public class MidtermProject{
 			
 			//If the answer is right, this code runs
 			impressedFabroa(con);
+			
+			//Gets the users choice course choice
+			strChoice = scene8(con,intMouseX,intMouseY,intMouseButtonClicked);
+			
+			//Clears the console for the next scene
+			con.clear();
+			
+			//If the player chooses Comp Sci as their course, this code runs
+			if(strChoice.equals("Comp Sci")){
+				//Runs scene 9
+				strResult = scene9(con);
+				
+				//Clears the console for the next scene
+				con.clear();
+				
+				//If they fail, this code runs
+				if(strResult.equals("Failed")){
+					dissapointedFabroa(con);	
+				}
+				
+			//If the player selects anything else, this code runs
+			}else{
+				//Runs scene 11
+				scene11(con);
+				
+				//Gets the users answer
+				intAnswer = con.readInt();
+				
+				//Clears the console for the next scene
+				con.clear();
+				
+				//If they do not answer 1, this code runs
+				if(intAnswer != 1){
+					dissapointedFabroa(con);
+				}
+			}
+			
+			//Draws scene 12
+			scene12(con);
 		}
 	}
 	
 	//Choice menu method that returns the choice that has been clicked (used for scenes 3, 8, 18, 19, and 20)
 	public static String choiceMenu(Console con, String strButton1, String strButton2, String strButton3, String strButton4, int intFontSize, int intMouseX, int intMouseY, int intMouseButtonClicked){
-		
 		//Defines and loads in the fonts
 		Font fntButtonFont;
 		Font fntRegularFont;
@@ -280,10 +331,13 @@ public class MidtermProject{
 		con.drawImage(imgImpressedFabroa,0,200);
 		
 		//Prints a description of the scene
-		con.println("Mr. Fabroa is impressed with your skills");
+		con.println("Mr. Fabroa is impressed with your skills!");
 		
 		//Pauses the console for 5 seconds
 		con.sleep(5000);
+		
+		//Clears the console for the next scene
+		con.clear();
 	}
 	
 	//Method to draw dissapointed fabroa (Used for scenes 7,10,15,21,25)
@@ -392,4 +446,124 @@ public class MidtermProject{
 		con.println("What is the answer to this equation? (round to the nearest tenth)");
 		con.print("> ");
 	}
+	
+	//Method for Scene 8 (Choose a subcommitee)
+	public static String scene8(Console con,int intMouseX,int intMouseY,int intMouseButtonClicked){
+		//Defines and initializes the choice variable
+		String strChoice;
+		strChoice = "";
+		
+		//Prints instructions for the player
+		con.println("Mr. Fabroa wants to know what course you would like to take");
+		con.println("Select the course you would like to take");
+		
+		//Gets the players choice
+		strChoice = choiceMenu(con,"Comp Sci","Comm Tech","Tech Design","None",35,intMouseX,intMouseY,intMouseButtonClicked);
+		
+		//Returns the players selected subcommittee
+		return strChoice;
+	}
+	
+	//Method for Scene 9 (Robot Minigame)
+	public static String scene9(Console con){
+		//Defines and loads the images for this scene
+		BufferedImage imgScene9;
+		BufferedImage imgRobot;
+		imgScene9 = con.loadImage("Scene 9.png");
+		imgRobot = con.loadImage("Robot.png");
+		
+		//Defines the variables for the robot movement
+		int intRobotX;
+		int intRobotY;
+		
+		//Defines the variable to read the current key pressed down
+		char chrCurrentCharacter;
+		
+		//Initializes the variables for the robot movement
+		intRobotX = 1039;
+		intRobotY = 500;
+		
+		//Draws the background for this scene
+		con.drawImage(imgScene9,0,200);
+		
+		//Prints the statements for this scene
+		con.println("Since you have coding experience, you can program this robot");
+		con.println("Using the W A S D keys on the keyboard, navigate the robot to the top right of the maze while\navoiding the borders!");
+		con.println("W - Up");
+		con.println("A - Left");
+		con.println("S - Down");
+		con.println("D - Right");
+		
+		//Infinite loop until a statement is returned to see if the user passes or fails this challenge
+		while(true){
+			//Draws a rectangle to get the old robot out of the scene
+			con.setDrawColor(Color.white);
+			con.fillRect(intRobotX,intRobotY,153,149);
+			chrCurrentCharacter = con.currentChar();
+			
+			//Logic to change the robot position
+			if(chrCurrentCharacter == 'w'){
+				intRobotY -= 5;
+			}else if(chrCurrentCharacter == 'a'){
+				intRobotX -= 5;
+			}else if(chrCurrentCharacter == 's'){
+				intRobotY += 5;
+			}else if(chrCurrentCharacter == 'd'){
+				intRobotX += 5;
+			}
+			
+			//If robot is touching the outermost border
+			if((intRobotY > 515) || (intRobotY < 245) || (intRobotX < 50) || (intRobotX > 1080)){
+				return("Failed");
+			}
+			
+			//If the robot is touching the middle line, this code runs
+			if((intRobotY >= 305) && (intRobotY <= 465)){
+				if(intRobotX > 99){
+					return("Failed");
+				}
+			}
+			
+			//If the robot gets to the top right, this code runs
+			if((intRobotY < 300) && (intRobotX > 985)){
+				return("Passed");
+			}
+			
+			//Redraws the image
+			con.drawImage(imgRobot,intRobotX,intRobotY);
+			con.repaint();
+			
+			//Makes the animation 30 fps
+			con.sleep(33);
+		}
+	}
+	
+	//Method for Scene 11 (Binary Question)
+	public static void scene11(Console con){
+		//Defines and loads the images for this scene
+		BufferedImage imgScene11;
+		imgScene11 = con.loadImage("Scene 11.png");
+		
+		//Draws the image for this scene
+		con.drawImage(imgScene11,0,200);
+		
+		//Prints the instructions for this scene
+		con.println("Since you are not taking Computer Science, answer this basic coding question to test your coding knowledge");
+		con.print("> ");
+	}
+	
+	//Method for Scene 12 (Welcome to robotics - Programming branch)
+	public static void scene12(Console con){
+		//Defines and loads the images for this scene
+		BufferedImage imgScene12;
+		imgScene12 = con.loadImage("Scene 12.png");
+		
+		//Prints a statement at the top of the screen
+		con.println("Congratulations! Welcome to robotics. You are now a programmer.");
+		
+		//Draws the scene image
+		con.drawImage(imgScene12,0,200);
+	}
+	
+	//
 }
